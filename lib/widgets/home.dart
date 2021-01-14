02063d322111c253b239acb1915270d50a929cc5
Florian Kogon <F.Kogon@gmail.com>
2021-01-14 22:22:41 +0100

    add-dismissible

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -12,6 +12,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  List<Activite> listOfActivities = [
+    Activite("Velo", Icons.directions_bike),
+    Activite("Golf", Icons.golf_course),
+    Activite("Peinture", Icons.palette),
+    Activite("Basketball", Icons.sports_basketball),
+    Activite("Video games", Icons.gamepad),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +27,71 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
+        child: ListView.builder(
+          itemCount: listOfActivities.length,
+          itemBuilder: (context, i) {
+            Activite activite = listOfActivities[i];
+            String key = activite.nom;
+            return Card(
+              elevation: 5.0,
+              child: Dismissible(
+                key: Key(key),
+                child: ListTile(
+                  title: Text("Activite : ${activite.nom}"),
+                  trailing: Icon(activite.icon),
+                ),
+                background: Container(
+                  padding: EdgeInsets.only(right: 15.0),
+                  color: Colors.red,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("Supprimer", style: TextStyle(color: Colors.white)
+                      ),
+                      Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+                onDismissed: (direction) {
+                  setState(() {
+                    listOfActivities.removeAt(i);
+                  });
+                },
+              ),
+            );
+          }),
+      ),
+      /* SINGLESCROLLVIEW
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(height: 100.0, color: Colors.red,),
+            Container(height: 100.0, color: Colors.blue,),
+            Container(height: 100.0, color: Colors.grey,),
+            Container(height: 100.0, color: Colors.green,),
+            Container(height: 100.0, color: Colors.red,),
+            Container(height: 100.0, color: Colors.blue,),
+            Container(height: 100.0, color: Colors.grey,),
+            Container(height: 100.0, color: Colors.green,),
           ],
         ),
       ),
+
+       */
     );
+  }
+}
+
+class Activite {
+  String nom;
+  IconData icon;
+
+  Activite(String nom, IconData icon) {
+    this.nom = nom;
+    this.icon = icon;
   }
 }
